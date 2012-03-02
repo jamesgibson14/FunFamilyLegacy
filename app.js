9a39bf6a5cd7b0bbac37b1929086befa3dac5d01
James Gibson <jamesgibson14@hotmail.com>
2012-03-02 09:58:56 -0700

    testing layout with Jade

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
-// Module dependencies.
 var express = require('express');
+var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
 
-var app = express.createServer();
+var app = module.exports = express.createServer();
 
-// Configuration
-app.configure( function() {
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
-// Routes
-app.get('/', function(req, res) {
-    res.send('Hello World');
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+var articleProvider= new ArticleProvider();
+
+app.get('/', function(req, res){
+    articleProvider.findAll( function(error,docs){
+        res.render('index.jade', { locals: {
+            title: 'Blog',
+            articles:docs
+            }
+        });
+    })
 });
 
 app.listen(80);
