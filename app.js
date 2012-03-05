57a2bfa0b24d71b839cac92cb4a168d60d42dc6f
server <server@ubuntuCompaq.(none)>
2012-03-04 23:44:22 -0700

    connected to MongoDb, add new blogs and comments to them

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+//var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
 
 var app = module.exports = express.createServer();
 
@@ -21,16 +22,62 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-var articleProvider= new ArticleProvider();
+var articleProvider= new ArticleProvider('localhost', 27017);
 
 app.get('/', function(req, res){
     articleProvider.findAll( function(error,docs){
-        res.render('index.jade', { locals: {
-            title: 'Blog',
-            articles:docs
+        res.render('index.jade', { 
+			locals: {
+	            title: 'Blog',
+	            articles:docs
             }
         });
     })
 });
+app.get('/blog/new', function(req, res) {
+    res.render('blog_new.jade', { locals: {
+        title: 'New Post'
+    }
+    });
+});
+
+app.get('/person/new', function(req, res) {
+    res.render('person_new.jade', { locals: {
+        title: 'New Person'
+    }
+    });
+});
+
+app.get('/blog/:id', function(req, res) {
+    articleProvider.findById(req.params.id, function(error, article) {
+        res.render('blog_show.jade',
+        { locals: {
+            title: article.title,
+            article:article
+        }
+        });
+    });
+});
+
+app.post('/blog/new', function(req, res){
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function( error, docs) {
+        res.redirect('/')
+    });
+});
+
+app.post('/blog/addComment', function(req, res) {
+    articleProvider.addCommentToArticle(req.param('_id'), {
+        person: req.param('person'),
+        comment: req.param('comment'),
+        created_at: new Date()
+       } , function( error, docs) {
+           res.redirect('/blog/' + req.param('_id'))
+       });
+});
+
 
 app.listen(80);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
